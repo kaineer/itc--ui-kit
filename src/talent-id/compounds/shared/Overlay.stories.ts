45ea337f85d50214f3dbf394d8cd159ef6cdf04f
Sergey Kluchkovsky <kaineer@gmail.com>
2026-01-27 15:33:16 +0500

    Выносит Overlay отдельно с onClick

---
+++ src/talent-id/compounds/shared/Overlay.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Overlay } from "./Overlay";
+import { fn } from "storybook/test";
+
+const meta = {
+  title: "TalentId/Kit/Overlay",
+  component: Overlay,
+  parameters: {},
+  argTypes: {
+    variation: { controls: "string" },
+  },
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof Overlay>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {},
+};
--- src/talent-id/compounds/shared/Overlay.tsx
+++ src/talent-id/compounds/shared/Overlay.tsx
@@ -1,15 +1,20 @@
 import clsx from "clsx";
 import classes from "./Overlay.module.css";
 import { getVariationClasses } from "../../shared/classes";
+import { type MouseEvent } from "react";
 
 interface Props {
   variation?: string;
+  onClick?: (e: MouseEvent<HTMLDivElement>) => null;
 }
 
-export const Overlay = ({ variation = "black-05" }: Props) => {
+export const Overlay = ({
+  variation = "black-05",
+  onClick = () => null,
+}: Props) => {
   const className = clsx(
     classes.overlay,
     getVariationClasses(variation, classes),
   );
-  return <div className={className}></div>;
+  return <div onClick={onClick} className={className}></div>;
 };
